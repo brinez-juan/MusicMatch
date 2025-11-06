f5fa44266eb4b384bdb3ff758eaa4c1ce6e7c32f
brinez-juan <A01782050@tec.mx>
2025-11-06 12:11:54 -0600

    RESTART

--- src/main.py
+++ src/main.py
@@ -1,16 +1,1 @@
-from auth import create_spotify_client
 
-def main():
-    sp = create_spotify_client()
-    user = sp.current_user()
-
-    print("\n✅ Conexión exitosa con Spotify API")
-    print("----------------------------------")
-    print(f"Nombre: {user['display_name']}")
-    print(f"País: {user['country']}")
-    print(f"Email: {user['email']}")
-    print(f"Seguidores: {user['followers']['total']}")
-    print(f"ID de usuario: {user['id']}")
-
-if __name__ == "__main__":
-    main()
