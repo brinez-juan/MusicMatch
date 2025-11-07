10c8c1be16a2cf8c284359c259d7d33eddc33f82
brinez-juan <A01782050@tec.mx>
2025-11-07 08:16:32 -0600

    Defined the function to fetch spotify data

--- src/spotify.py
+++ src/spotify.py
@@ -1,24 +1,41 @@
-# ✅ Create Spotify client ONCE
-from spotipy.oauth2 import SpotifyOAuth
-import spotipy
+from spotipy.oauth2 import SpotifyOAuth #spotify authentication library
+import spotipy #spotify functions library
 
+# Configuration variables
 from config import ( 
     SPOTIPY_CLIENT_ID, 
     SPOTIPY_CLIENT_SECRET, 
     SPOTIPY_REDIRECT_URI
 )
 
+""" 
+Spotify authentication - alows access to Spotify API 
+
+    Takes the client ID, client secret, and redirect URI from the config file wich are 
+    obtained from the Spotify Developer Dashboard when creating an app.
+
+    Defines the scope of access required, in this case to read user private data, email, 
+    top tracks, and library.
+
+"""
+
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     client_id=SPOTIPY_CLIENT_ID,
     client_secret=SPOTIPY_CLIENT_SECRET,
     redirect_uri=SPOTIPY_REDIRECT_URI,
-    scope="user-read-private user-read-email",
+    scope="user-read-private user-read-email user-top-read user-library-read",
     cache_path=".spotipy_cache"
 ))
 
-########################
-# 2. Fetch Spotify metadata
-########################
+"""
+Retrieve metadata for a Spotify track by its ID.
+
+    The function recieves the Spotify track ID as input and uses the Spotipy library
+    to return metadata such as title, artist, popularity, duration, release date, and 
+    album name.
+
+"""
+
 def fetch_spotify_metadata(spotify_track_id):
     metadata = sp.track(spotify_track_id)
     return {
@@ -26,5 +43,12 @@ def fetch_spotify_metadata(spotify_track_id):
         "artist": metadata["artists"][0]["name"],
         "popularity": metadata["popularity"],
         "duration_ms": metadata["duration_ms"],
-        "release_date": metadata["album"]["release_date"]
+        "release_date": metadata["album"]["release_date"],
+        "album": metadata["album"]["name"]  
     }
+
+
+# TEST CALL
+if __name__ == "__main__":
+    test_id = "0VjIjW4GlUZAMYd2vXMi3b"  # Blinding Lights
+    print(fetch_spotify_metadata(test_id))
