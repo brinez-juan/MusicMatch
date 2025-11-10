d61228ab35a6c0c6f73bebcadb7b92b170fc4c46
brinez-juan <A01782050@tec.mx>
2025-11-10 12:47:47 -0600

    deleted unused function

--- src/soundNet.py
+++ src/soundNet.py
@@ -30,28 +30,6 @@ def fetch_soundnet(track_id):
 
     raise Exception(f"SoundNet error {r.status_code} — {r.text}")
 
-
-"""
-def build_numeric_vector(soundnet_json):
-    tempo = float(soundnet_json.get("tempo", 0))
-    energy = float(soundnet_json.get("energy", 0)) / 100
-    dance = float(soundnet_json.get("danceability", 0)) / 100
-    happy = float(soundnet_json.get("happiness", 0)) / 100
-    acoustic = float(soundnet_json.get("acousticness", 0)) / 100
-    inst = float(soundnet_json.get("instrumentalness", 0)) / 100
-    live = float(soundnet_json.get("liveness", 0)) / 100
-    speech = float(soundnet_json.get("speechiness", 0)) / 100
-
-    loud_str = soundnet_json.get("loudness", "-60 dB")
-    try:
-        loud = float(loud_str.replace(" dB", ""))
-    except:
-        loud = -60.0
-
-    loud_norm = (loud + 60) / 60.0
-
-    return np.array([tempo, energy, dance, happy, acoustic, inst, live, speech, loud_norm], dtype=float)
-"""
 # TEST CALL
 if __name__ == "__main__":
     test_id = "0VjIjW4GlUZAMYd2vXMi3b"  
